5ae976fb4e04efcd790469597aff927063ff9a0f
rmessett15 <rmessett15@gmail.com>
2023-10-02 23:36:42 -0500

    added more todo list notes to reducer.js file

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
-    // TODO: Set up combine reducers
+  // TODO: Set up combine reducers
+  // https://laurenyz.medium.com/redux-with-hooks-basic-setup-6c1ea756d4cc -> step 7 to continue...
+    // Set up new functions that live above root reducer to set up initial state of each component and any subsequent components needed from a particular page
+
+  // Use reducers to create a more manageable stack of API calls
+    
 });
 
 export default rootReducer;
